7a920c755be2058e0c0bb19032765d60ee1a5b71
olesya <h_lesya00@mail.ru>
2022-08-09 11:20:09 +0300

    W32

---
+++ DZ_5_Work_32/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] GetArray(int n)
+{
+    int[]arr = new int[n];
+    Random rnd = new Random();
+
+    for (int i = 0; i<n; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+void PrintMassive(int[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[]arr = GetArray(size);
+
+PrintMassive(arr);
+
+int colChetDigit = 0;
+
+for (int i = 0; i<arr.Length; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        colChetDigit++;
+    }
+}
+Console.WriteLine("Колличество четных терхзначных: " + colChetDigit);
+
